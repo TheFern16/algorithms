d28e00c32a793474d6b45da33dec44db1671b3e1
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-09 09:27:27 -0700

    implement exponent rec function

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -18,10 +18,6 @@ function countDownRec(number) {
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
 function exponent(base, expo) {
-  // currentVal
-  // number of expo
-  // while number of expo < expo
-    // currentVal = currentVal * base
   let currentVal = base;
   let numberOfExpo = 1;
   while (numberOfExpo < expo) {
@@ -32,6 +28,20 @@ function exponent(base, expo) {
 }
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+function exponentRec(base, expo) {
+  let result = 1;
+
+  (function rec(currentExp) {
+    if (currentExp === 0) {
+      return;
+    } else {
+      result = result * base;
+      return rec(currentExp - 1);
+    }
+  })(expo);
+
+  return result;
+}
 
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
 

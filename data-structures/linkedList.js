604a253440876cf3c3bcc73e395b8ff97b2a77fb
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-11 09:50:21 -0700

    implement removebefore method

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -209,10 +209,24 @@ LinkedList.prototype.insertBefore = function(node, value) {
 };
 // Time complexity: O(n);
 
+
+// removeBefore 5
+// 3 4 5 6 7
+// 3    5 6 7
 LinkedList.prototype.removeBefore = function(node) {
-  // implement me...
+  let currentNode = this.head;
+
+  while (currentNode) {
+    if (currentNode.next.next.value === node.value && node.value !== this.head) {
+      let temp = currentNode.next.next;
+      currentNode.next = temp;
+      return;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
 };
-// Time complexity:
+// Time complexity: O(n);
 
 
 

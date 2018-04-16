de1d0d8f5b48606d6955d89c6d65c08549a73707
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-16 09:49:16 -0700

    implement flatten function

--- recursion/flatten.js
+++ recursion/flatten.js
@@ -5,3 +5,12 @@ flatten([1,[2],[3, [[4]]]]);
 => [1,2,3,4]
 */
 
+function flatten(arr = []) {
+  return arr.reduce((res, val) => {
+    if (Array.isArray(val)) {
+      return res.concat(flatten(val));
+    } else {
+      return res.concat(val);
+    }
+  }, []);
+}

f7e7d3ff0780f2e244532ac70289a225f4b8b860
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-16 09:46:22 -0700

    implement factorial function

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -4,3 +4,10 @@ Implement factorial.
 factorial(5) => 5*4*3*2*1 => 120
 */
 
+function factorial(num = 1, result = 1) {
+  if (num === 1) {
+    return result;
+  } else {
+    return factorial(num -= 1, result = result * num);
+  }
+};

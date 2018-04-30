9526ed6b4e31bd69ad5565225be28b55ad9de13a
woonandez <matthew.allen.fernandez@gmail.com>
2018-04-30 08:37:54 -0700

    implement countdownrec function

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -1,7 +1,16 @@
 //1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
+function countDown(num = 0) {
+  while (num > 0) {
+    console.log(num);
+    num--;
+  }
+}
 
 //2. Next, try looping just like above except using recursion
-
+function countDownRec(num = 0) {
+  console.log(num);
+  return num === 0 ? num : countDownRec(num-1);
+}
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
 
@@ -13,9 +22,3 @@
 
 //6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
 
-function countDown(num = 0) {
-  while (num > 0) {
-    console.log(num);
-    num--;
-  }
-}

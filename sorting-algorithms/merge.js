ef70d879d54aa00f789cd6ed775880075912f866
woonandez <matthew.allen.fernandez@gmail.com>
2018-05-01 11:28:00 -0700

    add pseudocode and desc for mergesort

--- sorting-algorithms/merge.js
+++ sorting-algorithms/merge.js
@@ -27,22 +27,58 @@ input array: [ 1 2 4 5 9 ]
 subarrays for regular merge sort: [ [1], [2], [4], [5], [9] ]
 subarrays for natural merge sort: [ [1,2], [4,5], [9] ]
 
-*/
+general plan
 
-function mergeSortRec(arr = []) {
+mergesortRec
+  if arr.length === 1 return arr;
+  split the array into two halves
+  left = arr.slice(0, arr.length / 2);
+  right = arr.slice(arr.length / 2)
+
+  sortLeft = split the two halfs again
+  sortRight = split the two halfs again
+
+  return whatever is returned from merge
+
+sortAndMerge(left, right)
+  // result
+  // leftIndex
+  // rightIndex
+  // base case (while the result length is < the lenght of the two arrays) keep going
+    // if the left index === the length of the left array
+      // then we know that we have gone through all of the left array
+      // therefore we can concat the rest of the right[rightIndex] to the result array
+      // this completes the while loop
+    // else if the right index === the length of the right array
+      // then we know that we have gone through all of the right array
+      // therefore we can concat the rest of the left[leftIndex] to the result array
+      // this completes the while loop
+    // else if left[leftIndex] <= right[rightIndex]
+      // push the left value into the result array (we always want the lower value)\
+      // increment the leftIndex
+      // while loop goes back around
+    // else
+      // push the right value into the result array (we always want the lower value)
+      // increment the rightIndex
+      // while loop goes back around
+
+
+*/
+function mergeSort(arr = []) {
   if (arr.length <= 1) return arr;
   const left = arr.slice(0, arr.length / 2);
   const right = arr.slice(arr.length / 2);
+
   const lSorted = mergeSortRec(left);
   const rSorted = mergeSortRec(right);
-  return merge(lSorted, rSorted);
+
+  return sortAndMerge(lSorted, rSorted);
 }
 
-function merge(l, r) {
+function sortAndMerge(l, r) {
   let result = [];
   let leftIndex = 0;
   let rightIndex = 0;
-
 
   while (result.length < (l.length + r.length)) {
     if (leftIndex === l.length) {

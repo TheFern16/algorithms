86f6512f720740f79639956bdd6feabdf81c802a
woonandez <matthew.allen.fernandez@gmail.com>
2018-05-02 08:23:20 -0700

    implement insert after method on linked list

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -109,9 +109,20 @@ LinkedList.prototype.print = function() {
 // Time complexity: O(n);
 
 LinkedList.prototype.insertAfter = function(node, value) {
-  // implement me...
+  let currentNode = this.head;
+  while (currentNode) {
+    if (currentNode.value === node.value) {
+      let temp = currentNode.next;
+      currentNode.next = new Node(value);
+      currentNode.next.next = temp;
+      return;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
 };
-// Time complexity:
+// Time complexity: O(n);
+
 
 LinkedList.prototype.removeAfter = function(node) {
   // implement me...
